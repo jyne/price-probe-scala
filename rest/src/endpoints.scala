2bb74fdc19f32847d33ffb3f4a84210ac4078cbb
andream16 <andrea.medda.uni@gmail.com>
2017-06-20 12:14:15 +0200

    Realized Mavedn Project for Akka Http

---
+++ rest/src/endpoints.scala
@@ -0,0 +1,44 @@
+/**
+  * Created by andream16 on 20.06.17.
+  */
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.Route
+import akka.stream.ActorMaterializer
+
+object endpoints {
+
+  import scala.io.StdIn
+
+    val host = "localhost"
+    val port = 8080
+
+    def main(args: Array[String]): Unit = {
+
+      implicit val system = ActorSystem("simple-rest-system")
+      implicit val materializer = ActorMaterializer()
+      implicit val executionContext = system.dispatcher
+
+      //Define the route
+      val route : Route = {
+        path("health") {
+          get {
+            complete(StatusCodes.OK,"Everything is great!")
+          }
+        }
+      }
+
+      //Startup, and listen for requests
+      val bindingFuture = Http().bindAndHandle(route, host, port)
+      println(s"Waiting for requests at http://$host:$port/...\nHit RETURN to terminate")
+      StdIn.readLine()
+
+      //Shutdown
+      bindingFuture.flatMap(_.unbind())
+      system.terminate()
+    }
+
+}
