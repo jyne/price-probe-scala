295e1699ecb3dd1776ff923a1f29d9361278a07b
andream16 <andrea.medda.uni@gmail.com>
2017-08-24 15:32:09 +0200

    Server configuration edits

--- src/main/scala/connection/src/SparkConnectionFactory.scala
+++ src/main/scala/connection/src/SparkConnectionFactory.scala
@@ -14,11 +14,11 @@ class SparkConnectionFactory {
   def initSparkConnection() : Unit = {
     val configuration = new SparkConf()
       .setAppName("price-probe")
-      //.setMaster("spark://" + remoteFactory.sparkSubnet + ":" + remoteFactory.sparkPort)
-      .setMaster("local[*]")
-      .set("spark.executor.host", remoteFactory.remoteHost)
-      .set("spark.executor.port", remoteFactory.sparkPort.toString)
-      .set("spark.cassandra.connection.host", remoteFactory.remoteHost)
+      .setMaster("spark://" + remoteFactory.sparkSubnet + ":" + remoteFactory.sparkPort)
+      //.setMaster("local[*]")
+      //.set("spark.executor.host", remoteFactory.remoteHost)
+      //.set("spark.executor.port", remoteFactory.sparkPort.toString)
+      .set("spark.cassandra.connection.host", remoteFactory.cassandraSubnet)
       .set("spark.cassandra.connection.port", remoteFactory.cassandraPort.toString)
     val sparkSession = SparkSession
       .builder()
